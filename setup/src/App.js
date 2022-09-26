bace6e349bc3a8a72ea2a42b1593cebf083c12ac
PaAmatJow <87549430+PaAmatJow@users.noreply.github.com>
2022-09-26 23:42:49 +0000

    initial commit

---
+++ setup/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project';
+function App() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [tours, setTours] = useState([]);
+
+	if (isLoading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	}
+}
+
+export default App;
