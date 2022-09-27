f9bdd647588f35f8949c4decafa5daa11bee7637
PaAmatJow <87549430+PaAmatJow@users.noreply.github.com>
2022-09-27 00:43:13 +0000

    completed

--- setup/src/App.js
+++ setup/src/App.js
@@ -5,8 +5,31 @@ import Tours from './Tours';
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project';
 function App() {
+	//states
 	const [isLoading, setIsLoading] = useState(true);
 	const [tours, setTours] = useState([]);
+	//functions
+	const removeTour = (id) => {
+		const newTours = tours.filter((tour) => tour.id !== id);
+		setTours(newTours);
+	};
+
+	const fetchTours = async () => {
+		setIsLoading(true);
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setIsLoading(false);
+			setTours(tours);
+		} catch (error) {
+			setIsLoading(false);
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
 
 	if (isLoading) {
 		return (
@@ -15,6 +38,27 @@ function App() {
 			</main>
 		);
 	}
+	if (tours.length === 0) {
+		return (
+			<div className='title'>
+				<h2>no tours left</h2>
+				<div className='underline'></div>
+				<button className='btn' onClick={fetchTours}>
+					refresh
+				</button>
+			</div>
+		);
+	}
+	return (
+		<main>
+			<Tours tours={tours} removeTour={removeTour} />
+			<div className='clr-btn-wrapper'>
+				<button className='btn' onClick={() => setTours([])}>
+					clear all
+				</button>
+			</div>
+		</main>
+	);
 }
 
 export default App;
